1d53f6fa7c4faac2d0b145c16a580e48b58070ed
Gabriel Mutano <gabrieloliveirabarros1997@gmail.com>
2024-10-28 11:49:58 -0300

    Att 0.5 Gabriel

--- SoftSkillsHub/scripts/script.js
+++ SoftSkillsHub/scripts/script.js
@@ -1,4 +1,20 @@
-// script.js
-
-// Você pode adicionar código JavaScript aqui para interações no seu site.
 console.log("Bem-vindo ao Soft Skills Hub!");
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('Scrolling', window.scrollY > 500) 
+    })
+
+
+window.addEventListener("scroll", function() {
+    let footer = document.querySelector('footer');
+    let scrollPosition = window.scrollY + window.innerHeight; // Posição atual do scroll mais a altura da janela
+    let pageHeight = document.documentElement.scrollHeight; // Altura total da página
+    
+    if (scrollPosition > pageHeight - 100) { // Altere '100' conforme necessário para ajustar a sensibilidade
+        footer.classList.add('ShowFooter');
+    } else {
+        footer.classList.remove('ShowFooter');
+    }
+});
+    
