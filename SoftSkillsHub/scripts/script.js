b40971f6013c4cbf507ff88f2cfe24b6f21fbfed
Gabriel Mutano <gabrieloliveirabarros1997@gmail.com>
2024-10-29 13:47:18 -0300

    Layout, animações e lorem

--- SoftSkillsHub/scripts/script.js
+++ SoftSkillsHub/scripts/script.js
@@ -17,4 +17,26 @@ window.addEventListener("scroll", function() {
         footer.classList.remove('ShowFooter');
     }
 });
-    
+
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btn-cadastro');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () =>{   
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
